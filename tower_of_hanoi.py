730e52726e500983ba558b635500cca12b439d65
insirenator <arsshakeeb149@protonmail,com>
2022-04-28 11:59:55 +0530

    Added draw_tower() method

--- tower_of_hanoi.py
+++ tower_of_hanoi.py
@@ -24,20 +24,27 @@ class Tower_Of_Hanoi:
 		self.rod_data[t][self.rod_state[t]] = disk
 		self.rod_state[t] -= 1
 
-	def print_disk(N) :
+	def print_disk(self,N) :
 		if N == 0 :
-			ch = '|'
+			ch = '||'
 		else :
 			ch = f"|{N}"
 
-		half_disk = f"{' '*(self.max_disks-N)}{'@'*N}"
+		left_half = f"{' '*(self.max_disks-N)}{'@'*N}"
+		right_half = f"{'@'*N}{' '*(self.max_disks-N)}" 
 
-		return f"{half_disk}{ch}{half_disk}"
+		return f"{left_half}{ch}{right_half}"
 
 	def draw_tower(self) :
 		'''Draw the tower on the console window'''
-		pass
+		print(f"{self.print_disk(0)} {self.print_disk(0)} {self.print_disk(0)} ")
 
+		rodA = self.rod_data['A']
+		rodB = self.rod_data['B']
+		rodC = self.rod_data['C']
+
+		for a,b,c in zip(rodA,rodB,rodC) :
+			print(f"{self.print_disk(a)} {self.print_disk(b)} {self.print_disk(c)}")
 
 
 
